e21f21c372fd71a9f55a024cbf8acf73c8ff3028
Federicaye <gallofederica58@gmail.com>
2024-03-06 16:14:28 +0100

    prezzo due decomali

--- ticket.js
+++ ticket.js
@@ -3,12 +3,12 @@ let distance = prompt('quanti km devi percorrere?');
 let price = distance * 0.21;
 console.log(price);
 if (age < 18) {
-    document.getElementById("ticket").innerHTML= 'il costo del biglietto è' (price * 0.8);
+    document.getElementById("ticket").innerHTML= 'il costo del biglietto è ' + (price * 0.8).toFixed(2);
     console.log(price * 0.8)
 }
 else if (age > 65) {
-    document.getElementById("ticket").innerHTML= 'il costo del biglietto è' (price * 0.6);
+    document.getElementById("ticket").innerHTML= 'il costo del biglietto è ' + (price * 0.6).toFixed(2);
 }
 else {
-    document.getElementById("ticket").innerHTML= 'il costo del biglietto è' (price);
+    document.getElementById("ticket").innerHTML= 'il costo del biglietto è ' + (price).toFixed(2);
 }
